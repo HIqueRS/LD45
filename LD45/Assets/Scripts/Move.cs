ea277038d8f343125db15347665440effe272b7c
Henrique <riquesouzalima_@hotmail.com>
2019-10-05 13:33:43 -0300

    Move

---
+++ LD45/Assets/Scripts/Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float Speed;
+    private Vector3 VecDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        VecDir = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        VecDir *= Speed;
+
+        GetComponent<CharacterController>().Move(VecDir * Time.deltaTime);
+    }
+}
