c18e1b94f3a09b04e538b6311904e95a2db3be22
Henrique <riquesouzalima_@hotmail.com>
2019-10-05 15:49:43 -0300

    Ejecting

--- LD45/Assets/Scripts/Move.cs
+++ LD45/Assets/Scripts/Move.cs
@@ -10,6 +10,11 @@ public class Move : MonoBehaviour
     private Rigidbody2D RB2D;
     private Vector2 MoveVel;
 
+    public GameObject Eject;
+    public bool Ejected;
+
+    private float Force = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +24,57 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(!Ejected)
+        {
+            Movement();
+            Ejecting();
+        }
+       
+    }
+
+    private void Movement()
+    {
         VecDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         MoveVel = VecDir.normalized * Speed;
 
-        RB2D.MovePosition( RB2D.position + MoveVel * Time.deltaTime);
+        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        diff.Normalize();
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+
+
+        RB2D.MovePosition(RB2D.position + MoveVel * Time.deltaTime);
+    }
+
+    private void Ejecting()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log(Force);
+            Force += 10;
+           
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            GameObject Proj = Instantiate(Eject, transform.GetChild(0).transform.position, Quaternion.identity);
+            if (Force > 750)
+            {
+                Force = 750;
+            }
+            Proj.GetComponent<Rigidbody2D>().AddForce(transform.up * Force);
+            Force = 0;
+
+            Ejected = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Parasite")
+        {
+            Destroy(collision.gameObject);
+            Ejected = false;
+        }
     }
 }
